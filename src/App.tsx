8352ecc757326b4f1b2cc92838e9e8d6e1bf2b52
Mohammad Faraz Khan <faraz.khan@alignedautomation.com>
2025-11-26 12:22:22 +0530

    intial vite app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>My First Vite + React App 🚀</h1>
+      <p>Deployed on Vercel.</p>
+    </div>
+  );
+}
+
+export default App
